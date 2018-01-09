c8bfde5f9346c6e862a47168c4a18495f5d0a3b8
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-01-09 12:00:39 +0000

    Exit with errorlevel

--- test/run_tests.bat
+++ test/run_tests.bat
@@ -3,3 +3,5 @@ call %~dp0..\..\..\..\config_env.bat
 set "path=C:\Instrument\Apps\EPICS\support\pugixml\master\bin\windows-x64;C:\Instrument\Apps\EPICS\support\curl\master\bin\windows-x64;C:\Instrument\Apps\EPICS\support\MySQL\master\bin\windows-x64;%path%"
 
 %python% %~dp0test_journal_parser.py
+
+if %errorlevel% neq 0 exit /b %errorlevel%
